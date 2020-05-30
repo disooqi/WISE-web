28fea99b71422898d52f67a88093f40d461db38a
Mohamed Eldesouki <disooqi@gmail.com>
2020-05-30 11:59:44 -0400

    Create the WSGI Entry Point

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+#!./venv python
+# -*- coding: utf-8 -*-
+"""
+WISE: Natural Language Platform to Query Knowledge bases
+"""
+__author__ = "Mohamed Eldesouki"
+__copyright__ = "Copyright 2020-29, GINA CODY SCHOOL OF ENGINEERING AND COMPUTER SCIENCE, CONCORDIA UNIVERSITY"
+__credits__ = ["Mohamed Eldesouki"]
+__maintainer__ = "CODS Lab"
+__email__ = "wise@cods.encs.concordia.ca"
+__created__ = "2020-05-30"
+
+
+from wise import app
+
+
+if __name__ == '__main__':
+    app.run()
+
